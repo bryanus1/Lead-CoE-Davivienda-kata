8202323fac2bbbc37638e826fcd67a0aea30c710
Brayan Sanjuan <bryan.sj175@gmail.com>
2024-05-07 15:54:13 -0500

    test:

--- backend/src/auth/command/sign-in.command.handler.spec.ts
+++ backend/src/auth/command/sign-in.command.handler.spec.ts
@@ -2,8 +2,10 @@ import { IUserRepository } from '@/users/interfaces/users.interfaces';
 import { SignInCommandHandler } from './sign-in.command.handler';
 import User from '@/users/models/users.model';
 import { IAuthRepository } from '../interfaces/auth.interfaces';
+import { BadRequestException } from '@nestjs/common';
 
 describe('SignInCommandHandler', () => {
+  let commandHandler: SignInCommandHandler;
   const userRepositoryMock: IUserRepository = {
     getByUsernameOrEmail: jest.fn().mockImplementation((data) => {
       return new Promise((resolve, reject) => {
@@ -26,17 +28,48 @@ describe('SignInCommandHandler', () => {
       return new Promise((resolve, reject) => {
         if (data === 2) reject(new Error('user not found'));
 
-        resolve('hala_mundo');
+        resolve('hola_mundo');
       });
     }),
   };
 
-  it('should be defined', () => {
-    const commandHandler = new SignInCommandHandler(
+  beforeEach(() => {
+    commandHandler = new SignInCommandHandler(
       userRepositoryMock,
       authRepositoryMock,
     );
+  });
 
+  it('should be defined', () => {
     expect(commandHandler).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    it('should user data when user logged', async () => {
+      const response = await commandHandler.signIn({
+        username: 'BrayanUs1',
+        password: 'hola_mundo',
+      });
+
+      expect(response.token).toBe('user logged');
+    });
+
+    it('should throw if user not exists', async () => {
+      await expect(
+        commandHandler.signIn({
+          username: 'BryanUs1',
+          password: 'hola_mundo',
+        }),
+      ).rejects.toThrow(BadRequestException);
+    });
+
+    it('should throw if password is incorrect', async () => {
+      await expect(
+        commandHandler.signIn({
+          username: 'BrayanUs1',
+          password: 'hello_word',
+        }),
+      ).rejects.toThrow(BadRequestException);
+    });
   });
 });
